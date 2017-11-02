98effd84ac5c164bab01c3866b559ee5403df7af
laecheng <chc286@ucsd.edu>
2017-11-02 04:09:37 -0700

    undacity project done, fix small bugs

---
+++ js/custom.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+  // add scroll spying to the navagation bar
+  $(".facebook-icon").on('click', function() {
+    $("#qrModal img").attr("src", "image/facebook_qr.jpg");
+    $("#qrModal .text-center").html("Scan the QR code or follow this "
+      +"<a target='_blank' href='https://www.facebook.com/cheng.chaoran'>link</a>");
+    $("#qrModal").modal();
+  });
+
+  $(".wechat-icon").on('click', function() {
+    $("#qrModal img").attr("src", "image/wechat_qr.jpg");
+    $("#qrModal").modal();
+  });
+
+  $(".github-icon").on('click', function() {
+    $("#qrModal img").attr("src", "image/QR_Code_github.jpg");
+    $("#qrModal .text-center").html("Scan the QR code or follow this "
+      +"<a target='_blank' href='https://github.com/laecheng'>link</a>");
+    $("#qrModal").modal();
+  });
+
+  $(".linkedin-icon").on('click', function() {
+    $("#qrModal img").attr("src", "image/QR_Code_linkedin.jpg");
+    $("#qrModal .text-center").html("Scan the QR code or follow this "
+      +"<a target='_blank' href='https://www.linkedin.com/in/chaorancheng/'>link</a>");
+    $("#qrModal").modal();
+  });
+
+  $('body').scrollspy({target: ".navbar-custom", offset: 50});
+
+  $(".navbar-custom").affix({offset: {top: $("header").outerHeight(true)} });
+  // add smooth scroll animation to the navagation bar
+  $(".navbar-custom a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 550, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+  // initialize the plugin for pinterest grid
+  $('#portfolio_grid').pinterest_grid({
+    no_columns: 4,
+    padding_x: 25,
+    padding_y: 40,
+    margin_bottom: 50,
+    single_column_breakpoint: 752
+  });
+  // dynamiclly change the text of the show more link
+  $(".link-toggle").click(function() {
+    if($(this).html() == "Show more") {
+      $(this).html("Show less");
+    } else {
+      $(this).html("Show more")
+    }
+  });
+  $(window).resize(function() {
+    var window_size = $(window).width();
+    if(window_size <= 752) {
+      $("#myCarousel").addClass("header_hide");
+      $(".navbar-custom").removeClass("affix-top");
+      $(".navbar-custom").addClass("fixed-top");
+    } else {
+      $("#myCarousel").removeClass("header_hide")
+      $(".navbar-custom").removeClass("fixed-top");
+    }
+  });
+});
