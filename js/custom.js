a55b92dad90a3c525249f813d5cd87863189ba59
laecheng <chc286@ucsd.edu>
2017-11-07 02:33:23 -0800

    polish

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,21 @@
 $(document).ready(function() {
-  // add scroll spying to the navagation bar
+  // Pop up the QR code modal when user click the icom
   $(".facebook-icon").on('click', function() {
     $("#qrModal img").attr("src", "image/facebook_qr.jpg");
     $("#qrModal .text-center").html("Scan the QR code or follow this "
       +"<a target='_blank' href='https://www.facebook.com/cheng.chaoran'>link</a>");
     $("#qrModal").modal();
   });
-
   $(".wechat-icon").on('click', function() {
     $("#qrModal img").attr("src", "image/wechat_qr.jpg");
     $("#qrModal").modal();
   });
-
   $(".github-icon").on('click', function() {
     $("#qrModal img").attr("src", "image/QR_Code_github.jpg");
     $("#qrModal .text-center").html("Scan the QR code or follow this "
       +"<a target='_blank' href='https://github.com/laecheng'>link</a>");
     $("#qrModal").modal();
   });
-
   $(".linkedin-icon").on('click', function() {
     $("#qrModal img").attr("src", "image/QR_Code_linkedin.jpg");
     $("#qrModal .text-center").html("Scan the QR code or follow this "
@@ -26,9 +23,16 @@ $(document).ready(function() {
     $("#qrModal").modal();
   });
 
+  // notity user the sections
   $('body').scrollspy({target: ".navbar-custom", offset: 50});
 
-  $(".navbar-custom").affix({offset: {top: $("header").outerHeight(true)} });
+  // dynamiclly affix the navbar
+  $('.navbar-custom').affix({
+      offset: {
+        top: function(){return $("header").height()}
+      }
+  });
+
   // add smooth scroll animation to the navagation bar
   $(".navbar-custom a").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
@@ -45,13 +49,14 @@ $(document).ready(function() {
       });
     }
   });
+  
   // initialize the plugin for pinterest grid
   $('#portfolio_grid').pinterest_grid({
     no_columns: 4,
     padding_x: 25,
     padding_y: 40,
     margin_bottom: 50,
-    single_column_breakpoint: 752
+    single_column_breakpoint: 751
   });
   // dynamiclly change the text of the show more link
   $(".link-toggle").click(function() {
